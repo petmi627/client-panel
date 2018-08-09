8fa45784c543f56bb8ee913563855c17eeebc005
Mike Peters <mike@petmi627.lu>
2018-08-09 20:42:54 +0200

    Generated Sidebar Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { ClientsComponent } from './components/clients/clients.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     DashboardComponent,
-    ClientsComponent
+    ClientsComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule
