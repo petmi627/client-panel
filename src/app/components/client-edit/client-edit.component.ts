8532a9f3ea6ab64dcc4b448576e86566ad77ef56
Mike Peters <mike@petmi627.lu>
2018-08-18 21:03:43 +0200

    Added update functionality

--- src/app/components/client-edit/client-edit.component.ts
+++ src/app/components/client-edit/client-edit.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {Client} from '../../models/client';
+import {FlashMessagesService} from 'angular2-flash-messages';
+import {ClientService} from '../../service/client.service';
+import {ActivatedRoute, Router} from '@angular/router';
 
 @Component({
   selector: 'app-client-edit',
@@ -6,10 +10,48 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./client-edit.component.css']
 })
 export class ClientEditComponent implements OnInit {
+    id: string;
+    client: Client = {
+        firstName: '',
+        lastName: '',
+        email: '',
+        phone: '',
+        balance: 0
+    };
+    disableBalanceOnEdit: boolean = true;
 
-  constructor() { }
+    constructor(
+        private flashMessage: FlashMessagesService,
+        private clientService: ClientService,
+        private router: Router,
+        private route: ActivatedRoute,
+    ) { }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+        // Get id from URL
+        this.id = this.route.snapshot.params['id'];
+        // Get client
+        this.clientService.getClient(this.id).subscribe(client => {
+            this.client = client;
+        });
+    }
+
+    onSubmit({value, valid}: {value: Client, valid: boolean})
+    {
+        if (!valid) {
+            this.flashMessage.show('Please fill out the form correctly',
+                {cssClass: 'alert-danger', timeout: 4000});
+        } else {
+            // Add id
+            value.id = this.id;
+            // Add a new Client
+            this.clientService.updateClient(value);
+            // Show Message
+            this.flashMessage.show('Client saved',
+                {cssClass: 'alert-success', timeout: 4000});
+            // Redirect to Dash
+            this.router.navigate([`/client/${this.id}`]);
+        }
+    }
 
 }
