12597b4ef76d9a3e1114c9a53a78739d96c60690
Mike Peters <mike@petmi627.lu>
2018-08-21 01:30:19 +0200

    Added Logout functionality

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {Client} from '../../models/client';
+import {FlashMessagesService} from 'angular2-flash-messages';
+import {ActivatedRoute, Router} from '@angular/router';
+import {AuthService} from '../../service/auth.service';
 
 @Component({
   selector: 'app-navbar',
@@ -6,10 +10,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
+    isLoggedIn: boolean = false;
+    loggedInUser: string;
+    showRegister: string;
 
-  constructor() { }
+    constructor(
+        private flashMessage: FlashMessagesService,
+        private authService: AuthService,
+        private router: Router
+    ) { }
 
   ngOnInit() {
+      this.authService.getAuth().subscribe(auth => {
+          if (auth) {
+              this.isLoggedIn = true;
+              this.loggedInUser = auth.email;
+          } else {
+            this.isLoggedIn = false;
+          }
+      });
+  }
+
+  onLogoutClick() {
+      this.authService.logout();
+      this.flashMessage.show('You are now logged out', {cssClass: 'alert-success', timeout: 4000});
+      this.router.navigate(['/login']);
   }
 
 }
--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -19,4 +19,8 @@ export class AuthService {
   getAuth() {
     return this.afAuth.authState.pipe(map(auth => auth));
   }
+
+  logout() {
+    this.afAuth.auth.signOut();
+  }
 }
