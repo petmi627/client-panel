5d655a62a96f585af4f15f3d3eba8ab51a72f8ce
Mike Peters <mike@petmi627.lu>
2018-08-22 01:46:41 +0200

    Added change Settings functionality

--- src/app/components/settings/settings.component.ts
+++ src/app/components/settings/settings.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {Settings} from '../../models/settings';
+import {SettingsService} from '../../service/settings.service';
+import {FlashMessagesService} from 'angular2-flash-messages';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-settings',
@@ -6,10 +10,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./settings.component.css']
 })
 export class SettingsComponent implements OnInit {
+  settings: Settings;
 
-  constructor() { }
+  constructor(
+      private settingsService: SettingsService,
+      private flashMessage: FlashMessagesService,
+      private router: Router
+  ) { }
 
   ngOnInit() {
+    this.settings = this.settingsService.getSettings();
+  }
+
+  onSubmit() {
+    this.settingsService.changeSettings(this.settings);
+    this.flashMessage.show('Settings saved', {cssClass: 'alert-success', timeout: 4000});
   }
 
 }
